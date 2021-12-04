802b750cb98d3712a7dc37c4e470b7ffe395057c
Alex Daily <alexanderdaily001@gmail.com>
2021-12-04 18:05:11 +0100

    fizzbuzz v2

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -3,6 +3,20 @@ fn main() {
 }
 
 fn fizzbuzz(n: i32) {
+    for number in (1..n+1) {
+        let mut statement = String::from("");
+        let mut divisible_test = |i, d, s| if i % d == 0 {
+            statement.push_str(s)
+        };
+
+        divisible_test(number, 3, "Fizz");
+        divisible_test(number, 5, "Buzz");
+
+        println!("{}: {}", number, statement);
+    }
+}
+
+fn fizzbuzzOld(n: i32) {
     let mut index = 1;
 
     loop {
