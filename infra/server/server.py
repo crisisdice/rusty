86bfc57702519aa44b159d3d902023793778838c
Alex Daily <alexanderdaily001@gmail.com>
2021-12-20 22:13:10 +0100

    fix up server

--- infra/server/server.py
+++ infra/server/server.py
@@ -1,11 +1,15 @@
 # pylint: disable=import-error
+import sys
+
 from controller import internal_get, internal_post
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-PORT=8080
+PORT=8000
 
 """
-    TODO
+    _____________________________
+    subclasses:
+        0: BaseHTTPRequestHandler
 """
 class Server(BaseHTTPRequestHandler):
     def __init__(self, request, client_addr, server):
@@ -22,12 +26,12 @@ class Server(BaseHTTPRequestHandler):
     def do_POST(self):
         try:
             self.send_code(201)
-            #response_headers = self.headers_as_dict()
-            request = self.rfile.read(int(self.headers_as_dict().get('Content-Length')))
-            self.wfile.write(internal_post(request))
+            content_length = int(self.headers_as_dict().get('Content-Length'))
+            request = self.rfile.read(content_length)
+            #self.wfile.write(internal_post(request))
+            self.wfile.write(request)
 
         except Exception as err:
-            # TODO logging
             print(err)
             self.send_code(500)
 
@@ -37,13 +41,17 @@ class Server(BaseHTTPRequestHandler):
         return dict([parse_header(header) for header in self.headers.as_string().split('\n') if header != ''])
 
 """
+    start server
+    ------------
 """
-def run(server_class=HTTPServer, handler_class=Server, port=PORT):
+def main(server_class=HTTPServer, handler_class=Server, port=PORT):
+    # TODO logging
     # TODO configuration
+    print(sys.argv)
     server_address = ("", port)
     httpd = server_class(server_address, handler_class)
     print(f'Listening on {port}...')
     httpd.serve_forever()
 
 if __name__ == "__main__":
-    run()
+    main()
