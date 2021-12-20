f41412ca4a0bc84411db36403cb7a56bb4c309b1
Alex Daily <alexanderdaily001@gmail.com>
2021-12-20 18:25:48 +0100

    unifiy api

--- infra/client/rust.py
+++ infra/client/rust.py
@@ -1,6 +1,10 @@
 from json import loads as decode
 
-def parse_rust(res):
+def parse_rust_external(res):
     json = decode(res)
     return (json.get('stdout') if json.get('success') 
-        else json.get('stderr') or 'No jsonponse')
+        else json.get('stderr') or 'No Response')
+
+def parse_rust(res):
+    json = decode(res)
+    return json.get('stdout') or json.get('stderr')
--- infra/server/server.py
+++ infra/server/server.py
@@ -8,11 +8,11 @@ class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(internal())
 
     def do_POST(self):
-        self.send_response(405)
-        self.end_headers()   
+        self.send_response(201)
+        self.end_headers()
+        self.wfile.write(internal())
 
 """
 """
