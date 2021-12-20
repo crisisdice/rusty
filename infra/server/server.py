e7acfe9507ac52c8c5b8830081ef28adf4310676
Alex Daily <alexanderdaily001@gmail.com>
2021-12-20 18:44:41 +0100

    trying to parse headers

--- infra/server/server.py
+++ infra/server/server.py
@@ -14,7 +14,7 @@ class Server(BaseHTTPRequestHandler):
         self.send_response(201)
         self.end_headers()
         #print(self.headers)
-        content_length = self.headers.split('\n')
+        content_length = self.headers.as_string()
         print(content_length)
 
         self.wfile.write(self.rfile.read(10))
